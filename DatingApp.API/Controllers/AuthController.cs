95414ca22043c258e97918eafce9253a97ed11ca
Standa <vajcoj@gmail.com>
2020-03-10 18:39:00 +0100

    remove exception

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -47,7 +47,7 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            throw new Exception("AHOJJJJJ");
+            //throw new Exception("AHOJJJJJ");
 
             var userFromRepo = await _repo.Login(userForLoginDto.Username
                 .ToLower(), userForLoginDto.Password);
