b25f0c0a32381969aa96ba03fd38368233448273
Standa <vajcoj@gmail.com>
2020-03-13 17:52:24 +0100

    Protect multiple routes with AuthGuard

--- DatingApp-SPA/src/app/_guards/auth.guard.ts
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -20,7 +20,7 @@ export class AuthGuard implements CanActivate {
     }
 
     this.alertify.error('You shall not pass!!!');
-    this.router.navigate(['/home']);
+    this.router.navigate(['/']);
   }
 
 }
