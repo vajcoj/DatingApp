c83ea780e7c400a7b4e6e7cfaaf4e861b1d63347
Standa <vajcoj@gmail.com>
2020-03-16 12:48:57 +0100

    Create UsersController, Add AutoMapper

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IDatingRepository _repository;
+        private readonly IMapper _mapper;
+
+        public UsersController(IDatingRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _repository.GetUsers();
+            
+            var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+            return Ok(usersToReturn);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _repository.GetUser(id);
+
+            var userToReturn = _mapper.Map<UserForDetailDto>(user);
+
+            return Ok(userToReturn);
+        }
+
+
+    }
+}
--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DatingApp.API.Data
 {
-    interface IDatingRepository
+    public interface IDatingRepository
     {
         void Add<T>(T entity) where T: class;
         void Delete<T>(T entity) where T : class;
