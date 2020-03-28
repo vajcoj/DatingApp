9c7af78fbace066a9d60f649b89f22baeccacf98
Standa <vajcoj@gmail.com>
2020-03-28 20:47:01 +0100

    Add Main photo to navbar, add user object to response on login

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class AuthService {
   baseUrl = environment.apiUrl + 'auth/';
   jwtHelper = new JwtHelperService();
   decodedToken: any;
+  currentUser: User;
 
   constructor(private http: HttpClient) { }
 
@@ -18,10 +20,11 @@ export class AuthService {
     return this.http.post(this.baseUrl + 'login', model)
       .pipe(
         map((response: any) => {
-          const user = response;
-          if (user) {
-            localStorage.setItem('token', user.token);
-            this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          if (response) {
+            localStorage.setItem('token', response.token);
+            localStorage.setItem('user', JSON.stringify(response.user));
+            this.decodedToken = this.jwtHelper.decodeToken(response.token);
+            this.currentUser = response.user;
           }
         })
       );
