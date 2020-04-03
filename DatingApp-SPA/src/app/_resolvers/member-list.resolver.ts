a61e1b935e23f3074caa39b116751dbe6f341123
Standa <vajcoj@gmail.com>
2020-04-03 13:11:05 +0200

    SPA - PAgination setup

--- DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -8,6 +8,8 @@ import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class MemberListResolver implements Resolve<User[]> {
+    pageNumber = 1;
+    pageSize = 5;
 
     constructor(
         private userService: UserService,
@@ -15,7 +17,7 @@ export class MemberListResolver implements Resolve<User[]> {
         private alertify: AlertifyService) { }
 
     resolve(route: ActivatedRouteSnapshot) : Observable<User[]> {
-        return this.userService.getUsers().pipe(
+        return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
             catchError(error => {
                 this.alertify.error('Problem retrieving data.');
                 this.router.navigate(['/home']);
